370ac41c7b957ffce9c6dfdb038e03d947075e9d
andersonPedro <andersonpedrodn@gmail.com>
2024-10-09 16:46:28 -0300

    Adicionando a imagem do galinheiro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './homepage/header/header.component';
+import { HomepageComponent } from "./homepage/homepage/homepage.component";
+import { FooterComponent } from "./homepage/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomepageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/homepage/homepage/homepage.component.ts
+++ src/app/homepage/homepage/homepage.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
+  mudancaPalavra: string = 'história';
+  palavras: string[] = ['história', 'pesquisa', 'ensino', 'premiações', 'legado', 'extensão'];
+  palavraIndex: number = 0;
 
+  ngOnInit(): void {
+    this.startWordRotation();
+  }
+
+  startWordRotation(): void {
+    setInterval(() => {
+      this.palavraIndex = (this.palavraIndex + 1) % this.palavras.length;
+      this.mudancaPalavra = this.palavras[this.palavraIndex];
+    }, 1000);
+  }
 }
