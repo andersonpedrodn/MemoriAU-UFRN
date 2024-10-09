8861e7445e644177afc0059dfdd814eb71d1f544
andersonPedro <andersonpedrodn@gmail.com>
2024-10-09 17:28:05 -0300

    Adicionado palavraDinamica

--- src/app/homepage/homepage/homepage.component.ts
+++ src/app/homepage/homepage/homepage.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
-  mudancaPalavra: string = 'história';
+  palavraDinamica: string = 'história';
   palavras: string[] = ['história', 'pesquisa', 'ensino', 'premiações', 'legado', 'extensão'];
   palavraIndex: number = 0;
+  isHiding: boolean = false
+
+  constructor(private cdr: ChangeDetectorRef) {
+    
+  }
 
   ngOnInit(): void {
+    console.log('ngOnINit chamado!')
     this.startWordRotation();
   }
 
   startWordRotation(): void {
     setInterval(() => {
-      this.palavraIndex = (this.palavraIndex + 1) % this.palavras.length;
-      this.mudancaPalavra = this.palavras[this.palavraIndex];
-    }, 1000);
+      this.isHiding = true;
+      this.cdr.detectChanges();
+
+      setTimeout(() => {
+        this.palavraIndex = (this.palavraIndex + 1) % this.palavras.length;
+        this.palavraDinamica = this.palavras[this.palavraIndex];
+        this.isHiding = false;
+        this.cdr.detectChanges();
+      }, 200);
+    }, 3000);
   }
+
+  
 }
