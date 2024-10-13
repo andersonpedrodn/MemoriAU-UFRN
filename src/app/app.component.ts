a509ecd59b44dac6970e6158e61a6a02b1014ce9
Anderson Pedro do Nascimento <andersonpedrodn@gmail.com>
2024-10-13 18:35:46 -0300

    Criação da linha do tempo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './homepage/header/header.component';
 import { HomepageComponent } from "./homepage/homepage/homepage.component";
 import { FooterComponent } from "./homepage/footer/footer.component";
+import { TimelineComponent } from './homepage/timeline/timeline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomepageComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, HomepageComponent, FooterComponent, TimelineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
