8e0bdfabcd6ad6706b226b32b3817a1147e4ea08
andersonPedro <andersonpedrodn@gmail.com>
2024-10-15 14:21:45 -0300

    Adicionado comportamento na dropdown

--- src/app/homepage/header/header.component.ts
+++ src/app/homepage/header/header.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isDropdownOpen = false;
 
+  toggleDropdown(state: boolean) {
+    this.isDropdownOpen = state;
+  }
 }
