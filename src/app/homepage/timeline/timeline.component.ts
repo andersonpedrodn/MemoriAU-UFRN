22a186adc352e961ee17e4269623656f5fec086c
Anderson Pedro do Nascimento <andersonpedrodn@gmail.com>
2024-11-30 12:54:43 -0300

    Foi importado todas as mídias do componente timeline

--- src/app/homepage/timeline/timeline.component.ts
+++ src/app/homepage/timeline/timeline.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-timeline',
@@ -13,145 +12,224 @@ export class TimelineComponent {
   //controle da visibilidade do modal
   isModalOpen: boolean = false;
 
+   
+  
+  
   //Imagens a serem exibididas no modal
-  historicoEscolarGrande = 'assets/images/imageHistoricoEscolarGrande.webp';
-  historicoEscolarGrandeVerso = 'assets/images/imageHistoricoEscolarGrandeVerso.webp'
+  imageUmEsquerda = [
+    { 
+      thumb: 'assets/images/imageHistoricoEscolar.webp', 
+      relatedImages: [
+        'assets/images/imageHistoricoEscolarGrande.webp', // Frente do histórico escolar
+        'assets/images/imageHistoricoEscolarGrandeVerso.webp'  // Verso do histórico escolar
+      ], 
+      title: 'Histórico escolar de aluna da primeira turma do CAU-UFRN.',
+      source: 'Fonte: Virgínia Araújo'
+    },
+  ];
+  imageDoisEsquerda = [
+      { 
+        thumb: 'assets/images/placaUfrn.webp', 
+        relatedImages: [
+          'assets/images/placaUfrnFull.webp', 
+        ], 
+        title: 'Descerramento de placa do Setor de Aulas Teóricas IV em 1978',
+        source: 'Fonte: Acervo BCZM'
+      }
+  ]
+  imageTresEsquerda = [
+      { 
+        thumb: 'assets/images/imageInstalacaoPlaca.webp', 
+        relatedImages: [
+          'assets/images/imageInstalacaoPlacaFull.webp', 
+        ], 
+        title: 'Instalação de placa do LABCON.',
+        source: 'Fonte: Virgínia Araújo'
+      }
+  ]
+  imageQuatroEsquerda = [
+      { 
+        thumb: 'assets/images/imageDoutoradoPPGAU.webp', 
+        relatedImages: [
+          'assets/images/imageDoutoradoPPGAUFull.webp', 
+        ], 
+        title: 'Doutorado Acadêmico em Arquitetura e Urbanismo (PPGAU-UFRN).',
+        source: ''
+      }
+  ]
+  imageCincoEsquerda = [
+      { 
+        thumb: 'assets/images/imageConvite40anos.webp', 
+        relatedImages: [
+          'assets/images/imageConvite40anosFull.webp', 
+        ], 
+        title: 'Doutorado Acadêmico em Arquitetura e Urbanismo (PPGAU-UFRN).',
+        source: ''
+      }
+  ]
+  imageSeisEsquerda = [
+      { 
+        thumb: 'assets/images/imageColacaoPandemia.webp', 
+        relatedImages: [
+          'assets/images/imageColacaoPandemiaFull.webp', 
+          'assets/images/imageColacaoPandemiaFull.webp  ', 
+        ], 
+        title: 'Doutorado Acadêmico em Arquitetura e Urbanismo (PPGAU-UFRN).',
+        source: 'YouTube CT'
+      }
+  ]
 
-  //variável para armazenar a imagem atual do modal
-  currentImage: string = this.historicoEscolarGrande
+  imageUmDireita   = [
+    { 
+      thumb: 'assets/images/imagePlantaCampus.webp  ', 
+      relatedImages: [
+        'assets/images/imagePlantaCampusFull.webp', // Frente do histórico escolar
+        'assets/images/imageDocumentoFull.webp'  // Verso do histórico escolar
+      ], 
+      title: 'Planta geral do campus.',
+      source: ''
+    },
+  ];
+  imageDoisDireita   = [
+    { 
+      thumb: 'assets/images/imageColacaoGrau.webp ', 
+      relatedImages: [
+        'assets/images/imageColacaoGrauFull.webp', // Capa do álbum colação de grau
+        'assets/images/imageConviteFormaturaFull.webp',  // Convite de formatura da primeira turma do CAU
+        'assets/images/imageConviteFormaturaVersoFull.webp'  // Convite de formatura da primeira turma do CAU verso
+      ], 
+      title: 'Capa do álbum de fotografias da colação de grau da UFRN de 1978 .',
+      source: 'Fonte: Acervo BCZM'
+    },
+  ];
+  imageTresDireita   = [
+    { 
+      thumb: 'assets/images/imageEntregaCertificado.webp ', 
+      relatedImages: [
+        'assets/images/imageEntregaCertificadoFull.webp', // Capa do álbum colação de grau
+        'assets/images/imageEntregaCertificado2Full.webp',  // Convite de formatura da primeira turma do CAU
+  
+      ], 
+      title: 'Entrega de certificados do Curso de especialização e estudos do habitat com ênfase na questão ambiental .',
+      source: 'Fonte: Eunádia Cavalcante'
+    },
+  ];
+  imageQuatroDireita   = [
+    { 
+      thumb: 'assets/images/imageCapaPrimeiraRevistaPenas.webp ', 
+      relatedImages: [
+        'assets/images/imageCapaPrimeiraRevistaPenasFull.webp', // Capa do álbum colação de grau
+      
+      ], 
+      title: 'Capa da primeira edição da Revista Penas .',
+      source: ''
+    },
+  ];
+  imageCincoDireita   = [
+    { 
+      thumb: 'assets/images/imageAberturaMestrado.webp ', 
+      relatedImages: [
+        'assets/images/imageAberturaMestradoFull.webp', // Capa do álbum colação de grau
+      
+      ], 
+      title: 'Abertura do Mestrado Profissional .',
+      source: 'Fonte: Virgínia Araújo'
+    },
+  ];
+  imageSeisDireita   = [
+    { 
+      thumb: 'assets/images/imageCapaPrimeiraRevistaProjetar.webp ', 
+      relatedImages: [
+        'assets/images/imageCapaPrimeiraRevistaProjetarFull.webp', // Capa do álbum colação de grau
+      
+      ], 
+      title: 'Capa da primeira edição da Revista Projetar .',
+      source: 'Fonte: '
+    },
+  ];
+  imageSeteDireita   = [
+    { 
+      thumb: 'assets/images/imageCertificadoAcreditacao.webp ', 
+      relatedImages: [
+        'assets/images/imageCertificadoAcreditacaoFull.webp', // Capa do álbum colação de grau
+        'assets/images/imageVisitaConselhoFull.webp', // Capa do álbum colação de grau
+      
+      ], 
+      title: 'Certificação de acreditação',
+      source: 'Fonte: CAU-BR '
+    },
+  ];
+  imageOitoDireita   = [
+    { 
+      thumb: 'assets/images/imageInauguracaoNEAU.webp ', 
+      relatedImages: [
+        'assets/images/imageInauguracaoNEAUFull.webp', // Capa do álbum colação de grau
+        'assets/images/imageInauguracao2NEAUFull.webp', // Capa do álbum colação de grau
+      
+      ], 
+      title: 'Certificação de acreditação',
+      source: 'Fonte: Glauce Albuquerque '
+    },
+  ];
+  imageNoveDireita   = [
+    { 
+      thumb: 'assets/images/imagePlacaPraca.webp ', 
+      relatedImages: [
+        'assets/images/imagePlacaPracaFull.webp', // Capa do álbum colação de grau
+        'assets/images/imagePlacaPraca2Full.webp', // Capa do álbum colação de grau
+      
+      ], 
+      title: 'Inauguracação da Placa da Praça',
+      source: 'Fonte: Conselho de Arquitetura e Urbanismo CAU-RN '
+    },
+  ];
+  imageDezDireita   = [
+    { 
+      thumb: 'assets/images/imageSolenidade.webp ', 
+      relatedImages: [
+        'assets/images/imageSolenidadeFull.webp', // Capa do álbum colação de grau
+        'assets/images/imageSolenidade2Full.webp', // Capa do álbum colação de grau
+      
+      ], 
+      title: 'Solenidade na Câmara Municipal de Natal',
+      source: 'Fonte: Verônica Macedo '
+    },
+  ];
+ 
+    //variável para armazenar a imagem atual do modal
+    currentImageIndex = 0; // Índice da imagem atualmente exibida no modal
+    currentImageSet: string[] = []; // Conjunto de imagens exibidas no modal
+    currentSource = ''; // Fonte da imagem atual
+    currentTitle = '';  // Título da imagem atual
 
-  showModal() {
+  showModal(image: any) { 
     this.isModalOpen = true;
-    this.currentImage = this.historicoEscolarGrande;
+    this.currentImageSet = image.relatedImages; // Define o conjunto de imagens relacionadas
+    this.currentImageIndex = 0; // Começa na primeira imagem do conjunto
+    this.currentSource = image.source; // Fonte dinâmica
+    this.currentTitle = image.title;  // Título dinâmico
   }
 
   closeModal() {
     this.isModalOpen = false;
+    this.currentImageSet = [];
   }
 
   //Avançar para a próxima imagem
-  nextImage(){
-    if (this.currentImage === this.historicoEscolarGrande){
-      this.currentImage = this.historicoEscolarGrandeVerso; //Avança para a segunda imagem
-    }else {
-      this.currentImage = this.historicoEscolarGrande
+  nextImage() {
+    if (this.currentImageIndex < this.currentImageSet.length - 1) {
+      this.currentImageIndex++;
     }
   }
+
 
   //Volta para a imagem anterior
-  prevImage(){
-    if(this.currentImage === this.historicoEscolarGrandeVerso){
-      this.currentImage = this.historicoEscolarGrande
-    }else{
-      this.currentImage = this.historicoEscolarGrandeVerso;
+  prevImage() {
+    if (this.currentImageIndex > 0) {
+      this.currentImageIndex--;
     }
   }
 
-  // constructor(private modalService: NgbModal){
-    
-  // }
-
-  // isModalVisible = false; //Controle da visibilidade do modal
-  // selectedImage: string = ''; //Caminho da imagem 
-  // selectedDescription: string = ''; //Descrição da imagem
-
-  // timelineItems = [
-  //   { 
-  //     year: 1978, 
-  //     image: 'assets/images/imageHistoricoEscolar.webp', 
-  //     alt: 'Planta geral do campus em 1978', 
-  //     description: 'Planta geral do campus em 1978',
-      
-  //   },
-
-  //   { year: 1979, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Ano 1973', 
-  //     description: 'Histórico escolar de aluna da primeira turma do CAU-UFRN',
-  //     credit: 'Virgínia Araújo'
-  //   },
-
-  //   { year: 1978, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'álbum de fotografia da colação de grau da UFRN de 1978', 
-  //     description: 'Capa do álbum de fotografia da colação de grau da UFRN de 1978',
-  //     credit: 'Acervo BCZM'
-  //   },
-  //   { year: 1978, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Imagem de dois homens colocando a placa do setor IV', 
-  //     description: 'Descerramento de placa do Setor de Aulas Teóricas IV em 1978',
-  //     credit: 'Acervo BCZM'
-  //   },
-  //   { year: 1978, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Imagem de mulher na frente da placa do LABCON', 
-  //     description: 'Instalação de placa do LABCON',
-  //     credit: 'Virgínia Araújo'
-  //   },
-  //   { year: 1978, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia com vários ex alunos na entrega de certificações do curso de especialização', 
-  //     description: 'Entrega de certificados do Curso de especialização e estudos do habitat com ênfase na questão ambiental.',
-  //     credit: 'Eunádia Cavalcante'
-  //   },
-  //   { year: 1978, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia com pessoas apresentando', 
-  //     description: 'Doutorado Acadêmico em Arquitetura e Urbanismo (PPGAU-UFRN)'
-  //   },
-  //   { year: 2006, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia com capa de revista', 
-  //     description: 'Capa da primeira edição da revista Penas'
-  //   },
-  //   { year: 2006, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia com pessoas apresentando', 
-  //     description: 'Abertura do Mestrado Profissional',
-  //     credit: 'Virgínia Araújo'
-  //   },
-      
-  //   { year: 2006, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Folheto de convite de comemoração dos 40 anos do CAU-UFRN', 
-  //     description: 'Convite de comemoração dos 40 anos do CAU-UFRN',
-  //   },
-  //   { year: 2006, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Folheto da revista Projetar', 
-  //     description: 'Capa da primeira edição da Revista Projetar ',
-  //   },
-  //   { year: 2006, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia de certificado', 
-  //     description: 'Certificado de acreditação',
-  //     credit: 'CAU-BR'
-  //   },
-  //   { year: 2021, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia de video chamada durante colação de grau no período de pandemia COVID-19', 
-  //     description: 'Colação de Grau 2021',
-  //     credit: 'YouTube do CT'
-  //   },
-  //   { year: 2021, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia da inauguração do NEAU com alunos e professores', 
-  //     description: 'Inauguração do NEAU',
-  //     credit: 'Glauce Albuquerque'
-  //   },
-  //   { year: 2021, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia da inauguração da Placa Prof. Paulo Nobre como forma de homanagem póstuma ao professor e aos 50 anos do curso de arquitetura e urbanismo da UFRN', 
-  //     description: 'Inauguração da Placa da Praça',
-  //     credit: 'Conselho de Arquitetura e Urbanismo CAU-RN'
-  //   },
-  //   { year: 2024, image: 'assets/img/ano1973.jpg', 
-  //     alt: 'Fotografia com pessoas na Câmara Municipal de Natal', 
-  //     description: 'Solenidade na Câmara Municipal de Natal',
-  //     credit: 'Verônica Macedo'
-  //   },
-      
-  // ];
-
-  
-  // Função para abrir o modal
-//   openModal(imageSrc: string, description: string, content: any) {
-//     this.selectedImage = imageSrc;
-//     this.selectedDescription = description;
-//     this.modalService.open(content);
-//   }
-
-// //  //Fechar modal
-//     closeModal() {
-//     this.isModalVisible = false;
-//     }
 } 
 
   
